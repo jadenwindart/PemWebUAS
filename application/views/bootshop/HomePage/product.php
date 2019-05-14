b6224c4c9d8041126ca8e4abeb024d318556fddf
AndrewTjanadi <andrewtjanadi@gmail.com>
2019-05-14 12:45:36 +0700

    Layouting

--- application/views/bootshop/HomePage/product.php
+++ application/views/bootshop/HomePage/product.php
@@ -2,12 +2,12 @@
 		
 			<?php foreach($products as $product){?>
 				<li class="span3">
-				  <div class="thumbnail">
-					<a  href="product_details.html"><img src="<?php echo base_url('assets/Prod_Img/'.$product['product_id'].".jpg")?>" alt="No Image Found" width="70px" height="70px"/></a>
+				  <div class="thumbnail" >
+					<a  href="product_details.html" style="text-align:center; "><img src="<?php echo base_url('assets/Prod_Img/'.$product['product_id'].".jpg")?>" alt="No Image Found" style="object-fit:contain; height:200px; width:200px;"/></a>
 					<div class="caption">
-					  <h5><?php echo $product['name']?></h5>
+					  <h5 style="height:60px; width:250px;"><?php echo $product['name']?></h5>
 					 
-					  <h4 style="text-align:center"><a class="btn" href="product_details.html"> <i class="icon-zoom-in"></i></a> <a class="btn" href="<?php echo base_url('index.php/Cart/Add/'.$product['product_id'])?>">Add to <i class="icon-shopping-cart"></i></a> <p style="color:orangered;">Rp.<?php echo $product['price']?></p></h4>
+					  <h4 style="text-align:center"><a class="btn" href="product_details.html" > <i class="icon-zoom-in"></i></a> <a class="btn" href="<?php echo base_url('index.php/Cart/Add/'.$product['product_id'])?>">Add to <i class="icon-shopping-cart"></i></a> <p style="color:orangered; margin-top:15px;">Rp.<?php echo $product['price']?></p></h4>
 					</div>
 				  </div>
 				</li>
