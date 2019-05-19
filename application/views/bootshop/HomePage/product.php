1bef5fd91ea358946fa787644a6cad2d1ae5b022
vincentkurnia <vincent.utama@student.umn.ac.id>
2019-05-19 13:05:02 +0700

    Update product.php

--- application/views/bootshop/HomePage/product.php
+++ application/views/bootshop/HomePage/product.php
@@ -3,7 +3,6 @@
 			<?php foreach($products as $product){?>
 				<li class="span3">
 				  <div class="thumbnail" >
-<<<<<<< HEAD
 					<a  href="<?php echo base_url("index.php/Home/ProductDetail/".$product['product_id'])?>" style="text-align:center; "><img src="<?php echo base_url('assets/Prod_Img/'.$product['product_id'].".jpg")?>" alt="No Image Found" style="object-fit:contain; height:200px; width:200px;"/></a>
 					<div class="caption">
 					  <h5 style="height:60px; width:250px;">
@@ -20,13 +19,6 @@
 						</h5>
 					 
 					  <h4 style="text-align:center"><a class="btn" href="<?php echo base_url("index.php/Home/ProductDetail/".$product['product_id'])?>" > <i class="icon-zoom-in"></i></a> <a class="btn" href="<?php echo base_url('index.php/Cart/Add/'.$product['product_id'])?>">Add to <i class="icon-shopping-cart"></i></a> <p style="color:orangered; margin-top:15px;">Rp.<?php echo $product['price']?></p></h4>
-=======
-					<a  href="product_details.html" style="text-align:center; "><img src="<?php echo base_url('assets/Prod_Img/'.$product['product_id'].".jpg")?>" alt="No Image Found" style="object-fit:contain; height:200px; width:200px;"/></a>
-					<div class="caption">
-					  <h5 style="height:60px; width:250px;"><?php echo $product['name']?></h5>
-					 
-					  <h4 style="text-align:center"><a class="btn" href="product_details.html" > <i class="icon-zoom-in"></i></a> <a class="btn" href="<?php echo base_url('index.php/Cart/Add/'.$product['product_id'])?>">Add to <i class="icon-shopping-cart"></i></a> <p style="color:orangered; margin-top:15px;">Rp.<?php echo $product['price']?></p></h4>
->>>>>>> master
 					</div>
 				  </div>
 				</li>
